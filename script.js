211b3222ca9a83cdfbfb245ba44f2535565e5159
Froggy <156987934+FroggysOrd@users.noreply.github.com>
2024-08-30 10:16:33 -0400

    Create script.js
    js file for site

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple JavaScript to handle the contact form submission
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Message sent! We will get back to you soon.');
+    this.reset();
+});
